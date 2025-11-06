47a27a4af6941c3b2e6730f46b5e032a5586e5b3
pedroServfaz <pedro.servfaz@gmail.com>
2025-11-06 15:28:41 -0300

    Componente InputText

---
+++ src/components/input-text.tsx
@@ -0,0 +1,85 @@
+import { type VariantProps, tv } from "tailwind-variants";
+import Icon from "./icon";
+import Text from "./text";
+
+export const inputTextContainerVariants = tv({
+  base: "flex flex-col gap-1 w-full", // ocupa toda a largura
+});
+
+export const inputTextWrapperVariants = tv({
+  base: `
+    border border-solid border-border-primary
+    bg-transparent rounded flex items-center gap-3
+    transition-colors duration-200
+    focus-within:border-border-active
+  `,
+  variants: {
+    size: {
+      md: "h-10 p-3",
+    },
+    disabled: {
+      true: "pointer-events-none opacity-50",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    disabled: false,
+  },
+});
+
+export const inputTextVariants = tv({
+  base: `
+    bg-transparent outline-none placeholder:text-placeholder
+    text-accent-paragraph flex-1 w-full
+    focus:outline-none focus:ring-0
+  `,
+});
+
+export const inputTextIconVariants = tv({
+  base: "fill-placeholder",
+  variants: {
+    size: {
+      md: "w-6 h-6",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+interface InputTextProps
+  extends VariantProps<typeof inputTextWrapperVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {
+  icon?: React.ComponentProps<typeof Icon>["svg"];
+  handling?: boolean;
+  error?: React.ReactNode;
+}
+
+export default function InputText({
+  size,
+  disabled,
+  className,
+  icon,
+  error,
+  ...props
+}: InputTextProps) {
+  return (
+    <div className={inputTextContainerVariants({ className })}>
+      <div className={inputTextWrapperVariants({ size, disabled })}>
+        {icon && (
+          <Icon svg={icon} className={inputTextIconVariants({ size })} />
+        )}
+        <input
+          className={inputTextVariants()}
+          disabled={disabled as boolean}
+          {...props}
+        />
+      </div>
+      {error && (
+        <Text variant="label-small" className="text-accent-red">
+          {error}
+        </Text>
+      )}
+    </div>
+  );
+}
