db3eb237ecbe9685204e587a0db853ef1ed5fe60
pedroServfaz <pedro.servfaz@gmail.com>
2025-12-05 11:39:32 -0300

    Componente PhotoWidget

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,11 +1,48 @@
-import { Outlet } from "react-router";
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
+import type { Photo } from "../contexts/photos/models/photo";
 export default function PageHome() {
   return (
-    <>
-      <Text>Página Inicial!</Text>
-      <hr />
-      <Outlet />
-    </>
+    <Container>
+      <div className="grid grid-cols-4 gap-9">
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget photo={{} as Photo} loading />
+      </div>
+    </Container>
   );
 }
